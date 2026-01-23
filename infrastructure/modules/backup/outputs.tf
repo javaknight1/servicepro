545235ce4322f943e241ffa9c4656f9a4e21c357
Rob Avery <robertaveryiv@gmail.com>
2026-01-22 22:17:18 -0500

    chore: Removed terraform and helm chart since we are moving to a different infra

--- infrastructure/modules/backup/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vault_arn" {
-  description = "Backup vault ARN"
-  value       = aws_backup_vault.main.arn
-}
-
-output "plan_id" {
-  description = "Backup plan ID"
-  value       = aws_backup_plan.main.id
-}
--- infrastructure/modules/ses/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "domain_identity_arn" {
-  description = "SES domain identity ARN"
-  value       = var.domain != "" ? aws_ses_domain_identity.main[0].arn : null
-}
-
-output "configuration_set_name" {
-  description = "SES configuration set name"
-  value       = var.domain != "" ? aws_ses_configuration_set.main[0].name : null
-}
